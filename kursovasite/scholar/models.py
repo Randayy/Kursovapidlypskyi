8b63e88174dd40d715c1a2659a02a95294ec595a
Roman Pidlipskiy <108235770+Randayy@users.noreply.github.com>
2023-11-24 03:11:36 +0200

    try

--- kursovasite/scholar/models.py
+++ kursovasite/scholar/models.py
@@ -5,3 +5,7 @@ class MyData(models.Model):
     full_name = models.CharField(max_length=100)
     google_scholar_id = models.CharField(max_length=100)
     interest_list = models.CharField(max_length=1000)
+    sub_title = models.CharField(max_length=1000, default="")
+    email = models.CharField(max_length=1000, default="")
+    image = models.ImageField(upload_to='images/')
+    
